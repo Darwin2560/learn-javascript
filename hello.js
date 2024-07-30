a315945a0d73c5514b0d270d8e939fcaf909fff9
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-07-30 02:04:57 +0200

    feat: This is a hello world script

---
+++ hello.js
@@ -0,0 +1,9 @@
+/**
+ * This is a hello world script.
+ */
+
+console.log('hello world');
+
+document.write('Hello, World!'); // This line writes 'Hello, World!' to the document body
+
+alert('Hello, World!'); // This line displays an alert with 'Hello, World!'
