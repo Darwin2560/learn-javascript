64f4095efb694b663079f4d5fd0492aabbf0513b
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-07-30 11:41:29 +0200

    feat: sum

---
+++ suma/suma.js
@@ -0,0 +1,8 @@
+let a = prompt('Ingrese el primer número');
+let b = prompt('Ingrese el segundo número');
+
+a = Number(a)
+b = Number(b)
+
+let sum = a + b;
+document.write('La suma es: ', sum); // 15
