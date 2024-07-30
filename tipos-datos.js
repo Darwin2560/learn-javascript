cefbe3ab3072f276d311b35d7d7663ece71ee7bd
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-07-30 02:26:24 +0200

    feat: data type

---
+++ tipos-datos.js
@@ -0,0 +1,21 @@
+// tipo indefinido
+let indefinidoTipo;
+console.log(indefinidoTipo); // undefined
+
+// tipo string
+let stringTipo = 'Hello, World!';
+console.log(stringTipo); // Hello, World!
+
+// tipo número
+let numeroTipo = 10;
+console.log(numeroTipo); // 10
+
+let numeroDecimal = 10.5;
+console.log(numeroDecimal); // 10.5
+
+// tipo booleano
+let booleanoTrue = true;
+console.log(booleanoTrue); // true
+
+let booleanoFalse = false;
+console.log(booleanoFalse); // false
