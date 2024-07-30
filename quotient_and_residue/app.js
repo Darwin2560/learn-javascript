d18d9ae765bac626bfd02fe3b9cd4aab774ce926
darwin ruiz <junior.ruiz.perez00@gmail.com>
2024-07-30 22:40:20 +0200

    feat: quotient and residue

---
+++ quotient_and_residue/app.js
@@ -0,0 +1,14 @@
+// Variables
+let n1, n2, cociente, residuo
+
+// Entrada de datos
+n1 = parseFloat(prompt("Ingrese el primer número"))
+n2 = parseFloat(prompt("Ingrese el segundo número"))
+
+// Operaciones
+cociente = n1 / n2
+residuo = n1 % n2
+
+// Salida de resultados
+document.write('Cociente: ' + cociente + '<br>')
+document.write('Residuo: ' + residuo)
