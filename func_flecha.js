d552acec50effb343f37e404fb64f150bdf625f6
jrdarwin <junior.ruiz.perez00@gmail.com>
2025-02-12 18:19:45 +0100

    feat: arrow function

--- func_flecha.js
+++ func_flecha.js
@@ -1,3 +1,4 @@
+console.clear();
 
 const sumar = (a,b)=>a+b;
 console.log(sumar(2,3)); // 5
@@ -7,3 +8,15 @@ const saludar = () => {
 }
 
 saludar();
+
+// other examples
+
+function greeting(name) {
+    return `Hello ${name}`
+}
+
+console.log(greeting('John Doe'));
+
+const mGreeting = name => `Hello ${name}`
+
+console.log(mGreeting('Jane Doe'));
