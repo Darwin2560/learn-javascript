41421c7fdf4180a7c18a2d99aeb4bc9799697ea1
jrdarwin <junior.ruiz.perez00@gmail.com>
2025-02-12 22:04:59 +0100

    feat: destructuring

---
+++ destructuring.js
@@ -0,0 +1,39 @@
+console.clear();
+
+// const name = "Jhon Manuel Lark";
+// const nameArr = name.split(' ');
+
+// console.log(nameArr);
+
+// let firstName = nameArr[0];
+// let middleName = nameArr[1];
+
+// console.log(firstName);
+// console.log(middleName);
+
+// let [firstName, ,lastName] = nameArr;
+
+// console.log(firstName);
+// console.log(lastName);
+
+// const person = {
+//     firstName: 'Jhon',
+//     middleName: 'Manuel',
+//     lastName: 'Lark',
+//     age: 28
+// }
+
+// let { firstName, lastName } = person; 
+
+// console.log(firstName);
+// console.log(lastName);
+
+let firstName = 'Jhon';
+let lastName = 'Lark';
+
+const personObj = {
+    firstName,
+    lastName
+}
+
+console.log(personObj);
